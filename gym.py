49e97c35aadab589b018e42f41554e258ddf66ad
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-08-15 23:42:33 +0200

    model trained and played the game

--- gym.py
+++ gym.py
@@ -1,7 +1,6 @@
 import gym
 import random
 import numpy as np
-import h5py
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
@@ -40,7 +39,6 @@ def some_random_games_first():
 
 
 # some_random_games_first()
-
 
 def initial_population():
     # [OBS, MOVES]
@@ -107,7 +105,7 @@ def initial_population():
     return training_data
 
 
-def neural_network_mode(input_size):
+def neural_network_model(input_size):
     network = input_data(shape=[None, input_size, 1], name='input')
 
     network = fully_connected(network, 128, activation='relu')
@@ -130,3 +128,49 @@ def neural_network_mode(input_size):
     model = tflearn.DNN(network, tensorboard_dir='log')
 
     return model
+
+
+def train_model(training_data, model=False):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1)
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size=len(X[0]))
+
+    model.fit({'input': X}, {'targets': y}, n_epoch=3, snapshot_step=500, show_metric=True, run_id='openai_learning')
+    return model
+
+
+training_data = initial_population()
+model = train_model(training_data)
+
+scores = []
+choices = []
+
+for each_game in range(10):
+    score = 0
+    game_memory = []
+    prev_obs = []
+    env.reset()
+
+    for _ in range(goal_steps):
+
+        env.render()
+        if len(prev_obs) == 0:
+            action = random.randrange(0, 2)
+        else:
+            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
+        choices.append(action)
+
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        game_memory.append([new_observation, action])
+        score += reward
+
+        if done:
+            break
+        scores.append(score)
+
+print('Average score', sum(score) / len(score))
+print('Choice 1:{},Choice 0:{}'.format(choices.count(1) / len(choices),
+      choices.count(0) / len(choices)))
